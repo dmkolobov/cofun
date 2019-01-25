bfb79f2b2815b1bd937346b144a8e77dad069173
Dmitry Kolobov <dmkolobov@gmail.com>
2019-01-24 20:15:12 -0700

    Figure out curses a little bit.

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,23 +5,57 @@ module Lib
 import UI.NCurses 
 import Curses
 
+import Control.Comonad
 import Data.Functor 
+
+import Tape 
+import Uni
+
+import Control.Monad.IO.Class
+
+shouldQuit :: Event -> Bool 
+shouldQuit e 
+  | EventCharacter 'q' <- e = True 
+  | EventCharacter 'Q' <- e = True 
+  | True                    = False
+
+shouldAdvance :: Event -> Bool 
+shouldAdvance e 
+  | EventCharacter 's' <- e = True 
+  | True                    = False
+
+-- Note that Curses is an instance of MonadIO. So we can use `liftIO` inside 
+-- a curses `do` block to lift regular IO actions to the `Curses` monad.
+
+data Direction = L | R 
+
+data ScanState
+  = ScanState { scanDirection :: Direction
+              }
+
+mainLoop :: Window 
+         -> Window
+         -> Curses ()
+mainLoop buf win 
+  = do 
+      e <- getEvent win Nothing 
+      case e of 
+        Just e | shouldQuit e    -> return () 
+        Just e | shouldAdvance e -> do 
+                                      ( row , col ) <- getCursor win
+                                      updateWindow win 
+                                      -- Here we define an update to the window. 
+                                        $ do
+                                            drawString "@"
+                                            moveCursor (row + 1) col
+                                      render 
+                                      mainLoop buf win
+        _                        -> do 
+                                      mainLoop buf win
 
 someFunc = runCurses 
          $ do 
              w <- window 
-             updateWindow w
-               $ do 
-                   drawBox (Just $ Glyph '#' []) (Just $ Glyph '#' [])
-                   drawString "hello, world."
-             render 
-             waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q') 
-
-waitFor :: Window -> (Event -> Bool) -> Curses () 
-waitFor w p = loop
-  where 
-    loop = do 
-             ev <- getEvent w Nothing 
-             case ev of 
-               Nothing  -> loop 
-               Just ev' -> if p ev' then return () else loop
+             b <- window
+             setEcho False
+             mainLoop b w 
