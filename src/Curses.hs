7f45da4a20da45b8ee1d30f0478b009113d3d339
Dmitry Kolobov <dmkolobov@gmail.com>
2019-01-24 17:46:43 -0700

    Begin implementing ncurses.

---
+++ src/Curses.hs
@@ -0,0 +1,6 @@
+module Curses where 
+
+import UI.NCurses
+
+window :: Curses Window 
+window = newWindow 80 80 0 0  
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,26 @@ module Lib
     ( someFunc
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import UI.NCurses 
+import Curses
+
+import Data.Functor 
+
+someFunc = runCurses 
+         $ do 
+             w <- window 
+             updateWindow w
+               $ do 
+                   drawBox (Just $ Glyph '#' []) (Just $ Glyph '#' [])
+                   drawString "hello, world."
+             render 
+             waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q') 
+
+waitFor :: Window -> (Event -> Bool) -> Curses () 
+waitFor w p = loop
+  where 
+    loop = do 
+             ev <- getEvent w Nothing 
+             case ev of 
+               Nothing  -> loop 
+               Just ev' -> if p ev' then return () else loop
